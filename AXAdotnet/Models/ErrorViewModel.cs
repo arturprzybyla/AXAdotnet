b646a994239bcb31e3156f539f8b6ed91072afae
Artur <arturprzybyla1@gmail.com>
2020-12-20 20:34:07 +0100

    Initial commit

---
+++ AXAdotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AXAdotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
